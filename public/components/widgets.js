7b6b892623c9945020ff74884f15b210f0e6ad13
Tao Pan <pantao@deakin.edu.au>
2022-01-22 04:57:48 +1030

    modify table style

--- public/components/widgets.js
+++ public/components/widgets.js
@@ -1,43 +1,68 @@
 let holder = "text holder"
 const createHero = res => {
     return `
-        <div class="container row">
-            <div class="col s12">
-                <p class="flow-text hero-title">Your Diet Plan</p>
-            </div>
-            <div class="col s12">
-                <div id="user-name" class="col s12 m2">
-                    <p>${holder}</p>
-                </div>
-                <div id="BMI-value" class="col s12 m5">
-                    <p>Your BMI is considered:</p>
-                    <p class="pColor">${holder}</p>
-                </div>
-                <div id="cal-value" class="col s12 m5">
-                    <p>Recommended Daily Calories:</p>
-                    <p class="pColor">${holder}</p>
+        <div class="container displayArea">
+            <p class="hero-title">Your Diet Plan:</p>
+            
+            <div class="heroDisplay">
+                <div class="row">
+                    <div id="user-name" class="col s12 m2">
+                        <p>${holder}</p>
+                    </div>
+                    <div id="BMI-value" class="col s6 m5">
+                        <p>Your BMI is considered:</p>
+                        <p class="pColor">${holder}</p>
+                    </div>
+                    <div id="cal-value" class="col s6 m5">
+                        <p>Recommended Daily Calories:</p>
+                        <p class="pColor">${holder}</p>
+                    </div>
                 </div>
             </div>
         </div>`
 }
 
+const cube = (flag) => {
+    let style = flag ? "cube1" : "cube2"
+    return `
+        <div class=${style}>
+            <h5 class="title">meal name</h5>
+            <p class="number">number</p>
+        </div>
+    `
+}
+
+const empCube = () => {
+    return `
+        <div class="cube2"></div>
+    `
+}
+
+const ordCube = () => {
+    return `
+        <div class="cube1">Order</div>
+    `
+}
+
 const createDailyTable = res => {
     return `
-        <table>
+        <table cellpadding="0" cellspacing="0" >
             <thread>
                 <tr>
                     <th>BREAKFAST</th>
-                    <th>SIDE/SNACKS</th>
+                    <th>LUNCH</th>
                     <th>DINNER</th>
                     <th>CALORIES</th>
+                    <th></th>
                 </tr>
             </thread>
             <tbody>
                 <tr>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${ordCube()}</td>
                 </tr>
             </tbody>
         </table>
@@ -50,35 +75,54 @@ const createWeeklyTable = res => {
             <thread>
                 <tr>
                     <th>BREAKFAST</th>
-                    <th>SIDE/SNACKS</th>
+                    <th>LUNCH</th>
                     <th>DINNER</th>
                     <th>CALORIES</th>
+                    <th></th>
                 </tr>
             </thread>
             <tbody>
                 <tr> <!-- monday -->
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${empCube()}</td>
                 </tr>
                 <tr> <!-- tuesday -->
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
+                    <td>${cube(0)}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${cube(1)}</td>
+                    <td>${empCube()}</td>
                 </tr>
                 <tr> <!-- wednesday -->
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${empCube()}</td>
                 </tr>
                 <tr> <!-- thursday -->
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
-                    <td>${holder}</td>
+                    <td>${cube(0)}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${cube(1)}</td>
+                    <td>${empCube()}</td>
+                </tr>
+                <tr> <!-- wednesday -->
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${cube(1)}</td>
+                    <td>${cube(0)}</td>
+                    <td>${ordCube()}</td>
+                </tr>
+                <tr> <!-- tal -->
+                    <td>${empCube()}</td>
+                    <td>${empCube()}</td>
+                    <td>${empCube()}</td>
+                    <td>${cube(1)}</td>
+                    <td>${empCube()}</td>
                 </tr>
             </tbody>
         </table>
